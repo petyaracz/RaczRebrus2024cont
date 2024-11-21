1fe37bcacb0cc93956d298efa99cb291d5390100
Péter Rácz <petermartonracz@gmail.com>
2024-11-21 02:17:06 +0100

    new ppl

--- script/analysis.R
+++ script/analysis.R
@@ -14,22 +14,40 @@ library(performance)
 
 d = read_tsv('dat/filtered_data.tsv')
 
+# whoops
 d = d |> 
-  mutate(log_odds_back_total = ifelse(log_odds_back_total == -Inf, NA, log_odds_back_total))
+  mutate(
+    log_odds_back_total = ifelse(log_odds_back_total == -Inf, NA, log_odds_back_total),
+    accept = as.factor(accept)
+    )
 
 length(unique(d$id))
+d |> 
+  count(is.na(log_odds_back_total))
 
 aggr = d |> 
-  count(stem,log_odds_back_total,log_odds_back_suffix,accept,suffix, sort = T) |> 
+  count(stem,log_odds_back_total,log_odds_back_suffix,accept,suffix,llfpm10, sort = T) |> 
   pivot_wider(names_from = accept, values_from = n, values_fill = 0)
 
-fit1 = glm(cbind(`TRUE`,`FALSE`) ~ log_odds_back_total, data = aggr, family = binomial)
+fit1 = glm(cbind(`TRUE`,`FALSE`) ~ log_odds_back_total + llfpm10, data = aggr, family = binomial)
 tidy(fit1)
-fit2 = lmer(rt ~ accept + log_odds_back_total + llfpm10 + (1|id) + (1|target), data = d)
+fit2 = glm(cbind(`TRUE`,`FALSE`) ~ log_odds_back_suffix + llfpm10, data = aggr, family = binomial)
 tidy(fit2)
-fit3 = lmer(rt ~ accept * log_odds_back_total + llfpm10 + (1|id) + (1|target), data = d)
-check_collinearity(fit3)
-tidy(fit3)
-fit4 = lmer(rt ~ accept * log_odds_back_total * llfpm10 + (1|id) + (1|target), data = d)
-check_collinearity(fit4) # no
+plot(compare_performance(fit1,fit2))
 
+fit3 = lmer(rt ~ accept + log_odds_back_total + llfpm10 + (1|id) + (1|target), data = d)
+fit4 = lmer(rt ~ log_odds_back_total + accept * llfpm10 + (1|id) + (1|target), data = d)
+fit5 = lmer(rt ~ accept * log_odds_back_total + llfpm10 + (1|id) + (1|target), data = d)
+check_collinearity(fit4)
+fit6 = lmer(rt ~ accept * log_odds_back_total + accept * llfpm10 + (1|id) + (1|target), data = d)
+check_collinearity(fit4) # ok...
+check_collinearity(fit5) # ok...
+plot(compare_performance(fit3,fit4,fit5,fit6))
+
+fit3b = lmer(rt ~ accept + log_odds_back_total + llfpm10 + (1|id) + (1+accept|target), data = d)
+fit3c = lmer(rt ~ accept + log_odds_back_suffix + llfpm10 + (1|id) + (1|target), data = d)
+fit3d = lmer(rt ~ accept + log_odds_back_total + suffix + llfpm10 + (1|id) + (1+accept|target), data = d)
+plot(compare_performance(fit3,fit3b,fit3c,fit3d))
+
+sjPlot::plot_model(fit3d, 'pred', terms = c('log_odds_back_total','accept'))
+sjPlot::plot_model(fit3d, 'pred', terms = c('llfpm10','suffix'))
