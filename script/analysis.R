ab24e9ecf3111eaad6afd9d4f15a28fce8d95c05
Péter Rácz <petermartonracz@gmail.com>
2024-11-29 17:34:30 +0100

    extraordinary results

--- script/analysis.R
+++ script/analysis.R
@@ -4,6 +4,7 @@ library(rstanarm)
 library(tidybayes)
 library(broom.mixed)
 library(scales)
+library(randomForest)
 
 d = read_tsv('dat/filtered_data.tsv')
 
@@ -14,54 +15,43 @@ count(d,id, sort = T)
 count(d,language)
 
 d2 = d |> 
-  mutate(
-    lang = ifelse(language %in% c('de','en','fr','la','yi'), language, 'other'),
-    lang = fct_relevel(lang, 'other'),
-    knn_scaled = rescale(knn),
-    yi_la_scaled = rescale(yi_la),
-    date_scaled = rescale(date),
-    n_size_scaled = rescale(neighbourhood_size),
-    logfreq_scaled = rescale(llfpm10),
-    stem_length_scaled = rescale(stem_length),
-    logodds_scaled = rescale(log_odds_adj),
-    stem_phonology = fct_relevel(stem_phonology, 'other')
-  ) |> 
-  select(rt,accept,log_odds_adj,lang,stem_length,llfpm10,suffix,neighbourhood_size,stem_phonology,stem_final_consonant_cluster,date_scaled,n_size_scaled,logfreq_scaled,stem_length_scaled,logodds_scaled,yi_la_scaled,knn_scaled,id,stem)
+ mutate(
+  lang = ifelse(language %in% c('de','en','fr','la','yi'), language, 'other'),
+  lang = fct_relevel(lang, 'other'),
+  knn_scaled = rescale(knn_2_weight),
+  yi_la_scaled = rescale(yi_la_weight),
+  date_scaled = rescale(date),
+  n_size_scaled = rescale(neighbourhood_size),
+  logfreq_scaled = rescale(llfpm10),
+  stem_length_scaled = rescale(stem_length),
+  logodds_scaled = rescale(log_odds_adj),
+  stem_phonology = fct_relevel(stem_phonology, 'other')
+ ) |> 
+ select(rt,accept,log_odds_adj,lang,stem_length,llfpm10,suffix,neighbourhood_size,stem_phonology,date_scaled,n_size_scaled,logfreq_scaled,stem_length_scaled,logodds_scaled,yi_la_scaled,knn_scaled,id,stem)
 
-nrow(d2[!complete.cases(d2),]) == 0 # mm
-
-d3 = d2[!complete.cases(d2),]
+nrow(d2[!complete.cases(d2),]) == 0 # mm yes mmm
 
 # 5) Specify exactly which analyses you will conduct to examine the main question/hypothesis.
 # (a) bayesian glm predicting yes/no from conditions with participant random intercept and word random intercept, weakly informative priors
 # (b) bayesian glm predicting rt from conditions with participant random intercept and word random intercept, weakly informative priors
 # loo used for model selection
 
-lm1a = stan_glmer(accept ~ logodds_scaled + (1|id), data = d2, family = binomial, cores = 4)
-lm1b = stan_glmer(accept ~ logodds_scaled + (1+logodds_scaled|id), data = d2, family = binomial, cores = 4)
-lm1c = stan_glmer(accept ~ suffix + lang + stem_phonology + date_scaled + n_size_scaled + logfreq_scaled + stem_length_scaled + (1|id), data = d2, family = binomial, cores = 4)
-lm1d = stan_glmer(accept ~ suffix + lang + stem_phonology + date_scaled + n_size_scaled + logfreq_scaled + stem_length_scaled + (1+ logfreq_scaled |id), data = d2, family = binomial, cores = 4)
+# hahaha no
 
-loo1a = loo(lm1a)
-loo1b = loo(lm1b)
-loo1c = loo(lm1c)
-loo1d = loo(lm1d)
-loo_compare(loo1a,loo1b)
-loo_compare(loo1a,loo1c)
-loo_compare(loo1c,loo1d)
-loo_compare(loo1b,loo1c)
+d3 = d2 |> 
+ count(accept,stem,log_odds_adj,lang,stem_length,llfpm10,suffix,neighbourhood_size,stem_phonology,date_scaled,n_size_scaled,logfreq_scaled,stem_length_scaled,logodds_scaled,yi_la_scaled,knn_scaled) |> 
+ pivot_wider(names_from = accept, values_from = n, values_fill = 0) |> 
+ mutate(log_odds_accept = log(`TRUE`+1)/log(`FALSE`+1))
 
-plot(lm1c, 'areas', regex = 'lang|phonology|scaled')
-plot(lm1b, 'areas', regex = 'odds_scaled$')
+d4 = d2 |> 
+  summarise(rt = mean(rt), .by = c(stem,accept,log_odds_adj,lang,stem_length,llfpm10,suffix,neighbourhood_size,stem_phonology,date_scaled,n_size_scaled,logfreq_scaled,stem_length_scaled,logodds_scaled,yi_la_scaled,knn_scaled))
 
-lm1a |> 
-  add_epred_draws(newdata = d2, ndraws = 1000) |> 
-  ungroup() |> 
-  summarise(
-    mean_epred = mean(.epred),
-    mean_accept = mean(accept),
-    .by = c(stem,log_odds_adj)
-    ) |> 
-  ggplot() +
-  geom_point(aes(log_odds_adj,mean_accept))
-  geom_point(aes(log_odds_adj,mean_epred))
+rf1 = randomForest(log_odds_accept ~ log_odds_adj + lang + stem_length + llfpm10 + suffix + neighbourhood_size + stem_phonology + date_scaled + n_size_scaled + logfreq_scaled + stem_length_scaled + logodds_scaled + yi_la_scaled + knn_scaled, data = d3)
+
+rf2 = randomForest(rt ~ log_odds_adj + lang + stem_length + llfpm10 + suffix + neighbourhood_size + stem_phonology + date_scaled + n_size_scaled + logfreq_scaled + stem_length_scaled + logodds_scaled + yi_la_scaled + knn_scaled, data = d4)
+
+importance(rf1)
+rf1
+importance(rf2)
+rf2
+# what
